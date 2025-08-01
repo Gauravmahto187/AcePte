8e4093428a7737e913d0691de2a754c38f978707
Gauravmahto187 <gauravmahto269@gmail.com>
2025-08-01 10:43:56 +0545

    fix: mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,17 +12,6 @@ const Header = () => {
               Ace PTE
             </span>
           </div>
-
-          {/* Navigation */}
-
-          {/* Mobile menu button */}
-          <div className="md:hidden">
-            <button className="text-white/90 hover:text-white p-2">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
         </div>
       </div>
     </header>
